51a894ac34afff9629afbc77c142b610e03f1dd5
Anton Lem <antonlem78@gmail.com>
2026-02-12 20:05:31 +0100

    Completed TypeMemberGrouperTest

---
+++ core/src/test/resources/test-cases/core/sorter/spoon/type-member-grouper/valid/TypeMemberGrouperFixture.java
@@ -0,0 +1,15 @@
+package io.github.lemon_ant.jharmonizer.core.sorter.spoon.type_member_grouper;
+
+public class TypeMemberGrouperFixture {
+
+    private int alpha;
+    private int bravo;
+
+    public void charlie() {
+        // Intentionally empty.
+    }
+
+    public int delta() {
+        return alpha + bravo;
+    }
+}
