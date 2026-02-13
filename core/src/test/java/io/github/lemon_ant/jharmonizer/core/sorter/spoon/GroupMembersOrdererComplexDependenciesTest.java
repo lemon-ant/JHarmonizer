c151a742eb55a691b603247c88988889f47e7583
Anton Lem <antonlem78@gmail.com>
2026-02-13 21:53:25 +0100

    Disabled GroupMembersOrdererComplexDependenciesTest

--- core/src/test/java/io/github/lemon_ant/jharmonizer/core/sorter/spoon/GroupMembersOrdererComplexDependenciesTest.java
+++ core/src/test/java/io/github/lemon_ant/jharmonizer/core/sorter/spoon/GroupMembersOrdererComplexDependenciesTest.java
@@ -17,6 +17,7 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.NonNull;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.CtTypeMember;
@@ -25,6 +26,7 @@ import spoon.reflect.declaration.CtTypeMember;
 class GroupMembersOrdererComplexDependenciesTest {
 
     @Test
+    @Disabled("TODO Flaky test! Debug it!!!")
     void orderMembersInsideGroups_alphaDepsAndAccessors_expectedStableOrder() {
         // Given
         URL fixtureResourceUrl =
