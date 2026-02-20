de1025b4fa870aea60c38be4eeaa15724faacc16
Anton Lem <antonlem78@gmail.com>
2026-02-20 17:56:34 +0100

    Complication of field tests

--- core/src/test/resources/test-cases/core/e2e/restructure/03-field-initializer-var-ref-chain/input/FieldInitializerVarRefChainSample.java
+++ core/src/test/resources/test-cases/core/e2e/restructure/03-field-initializer-var-ref-chain/input/FieldInitializerVarRefChainSample.java
@@ -1,15 +1,21 @@
 package e2e;
 
 public class FieldInitializerVarRefChainSample {
-    int c = 1;
-    int b = c + 3;
-    int a = b + 9;
+    int g = 25;
+    int i = g + 27;
+    int f = 21 + g;
+    int e = 15;
+    int h = 1;
+    int d = h + 3;
+    int b = d + 9;
+    int c = 5;
+    int a = 12;
 
     public static void main(String[] args) {
         FieldInitializerVarRefChainSample sample = new FieldInitializerVarRefChainSample();
-        if (sample.a != 13 || sample.b != 4 || sample.c != 1) {
+        if (sample.b != 13 || sample.d != 4 || sample.h != 1) {
             throw new IllegalStateException(
-                    "Unexpected field chain values: a=" + sample.a + ", b=" + sample.b + ", c=" + sample.c);
+                    "Unexpected field chain values: b=" + sample.b + ", d=" + sample.d + ", h=" + sample.h);
         }
     }
 }
--- core/src/test/resources/test-cases/core/e2e/restructure/09-field-initializer-explicit-this-forward-chain/input/FieldInitializerInstanceRefChainSample.java
+++ core/src/test/resources/test-cases/core/e2e/restructure/09-field-initializer-explicit-this-forward-chain/input/FieldInitializerInstanceRefChainSample.java
@@ -1,15 +1,21 @@
 package e2e;
 
 public class FieldInitializerInstanceRefChainSample {
-    int c = this.b + 1;
-    int b = this.a + 3;
-    int a = this.c + 9;
+    int h = this.e + 1;
+    int e = this.b + 3;
+    int b = this.h + 9;
+    int c = e + 5;
+    int f = this.g + 9;
+    int d = this.g + 11;
+    int g = 15;
+    int i = this.a + 17;
+    int a  = 0; // TODO Exception
 
     public static void main(String[] args) {
         FieldInitializerInstanceRefChainSample sample = new FieldInitializerInstanceRefChainSample();
-        if (sample.a != 10 || sample.b != 3 || sample.c != 1) {
+        if (sample.b != 10 || sample.e != 3 || sample.h != 1) {
             throw new IllegalStateException(
-                "Unexpected field chain values: a=" + sample.a + ", b=" + sample.b + ", c=" + sample.c);
+                "Unexpected field chain values: b=" + sample.b + ", e=" + sample.e + ", h=" + sample.h);
         }
     }
 }
