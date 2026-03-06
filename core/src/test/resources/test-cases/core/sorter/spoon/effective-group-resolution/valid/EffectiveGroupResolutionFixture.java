dfe67f77b42cb82808f539cc19d8641bed4e5a38
AntonLem <antonlem78@gmail.com>
2026-03-06 22:09:03 +0100

    Update effective-group fixture to avoid constant-edge filtering

--- core/src/test/resources/test-cases/core/sorter/spoon/effective-group-resolution/valid/EffectiveGroupResolutionFixture.java
+++ core/src/test/resources/test-cases/core/sorter/spoon/effective-group-resolution/valid/EffectiveGroupResolutionFixture.java
@@ -2,7 +2,7 @@ package io.github.lemon_ant.jharmonizer.core.sorter.spoon;
 
 public class EffectiveGroupResolutionFixture {
 
-    public static final int PROVIDER = 1;
+    public static final int PROVIDER = Integer.parseInt("1");
     public static final int DIRECT_DEPENDENT = PROVIDER + 1;
     public static final int TRANSITIVE_PROVIDER = PROVIDER + 10;
     public static final int TRANSITIVE_DEPENDENT = TRANSITIVE_PROVIDER + 1;
