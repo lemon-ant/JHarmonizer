9cbbf58e697ca0b67044ccbd91354bed9696d08b
AntonLem <antonlem78@gmail.com>
2026-03-07 15:55:13 +0100

    Add E2E fixture scenario for blank-final optimized provider gap

---
+++ core/src/test/resources/test-cases/core/e2e/restructure/14-blank-final-optimized-provider-pending/input/BlankFinalOptimizedProviderPendingSample.java
@@ -0,0 +1,39 @@
+package io.github.lemon_ant.jharmonizer.core.e2e;
+
+public class BlankFinalOptimizedProviderPendingSample {
+    private static final int STATIC_BLANK_FINAL;
+
+    static {
+        if (false) {
+            STATIC_BLANK_FINAL = -1;
+        }
+    }
+
+    static {
+        STATIC_BLANK_FINAL = 10;
+    }
+
+    private static final int B_STATIC_READ = STATIC_BLANK_FINAL + 1;
+
+    private final int INSTANCE_BLANK_FINAL;
+
+    {
+        if (false) {
+            INSTANCE_BLANK_FINAL = -1;
+        }
+    }
+
+    {
+        INSTANCE_BLANK_FINAL = 20;
+    }
+
+    private final int A_INSTANCE_READ = INSTANCE_BLANK_FINAL + 1;
+
+    public static void main(String[] args) {
+        BlankFinalOptimizedProviderPendingSample sample = new BlankFinalOptimizedProviderPendingSample();
+        if (B_STATIC_READ != 11 || sample.A_INSTANCE_READ != 21) {
+            throw new IllegalStateException("Unexpected values: B_STATIC_READ=" + B_STATIC_READ
+                    + ", A_INSTANCE_READ=" + sample.A_INSTANCE_READ);
+        }
+    }
+}
