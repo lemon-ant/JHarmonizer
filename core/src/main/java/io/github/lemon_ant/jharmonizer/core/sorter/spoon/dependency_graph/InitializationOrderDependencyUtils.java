dc232c82f9b9ffc4f0d8d0a259b3c55d83e2a795
AntonLem <antonlem78@gmail.com>
2026-03-09 13:56:03 +0100

    Shorten blank-final write helper method name

--- core/src/main/java/io/github/lemon_ant/jharmonizer/core/sorter/spoon/dependency_graph/InitializationOrderDependencyUtils.java
+++ core/src/main/java/io/github/lemon_ant/jharmonizer/core/sorter/spoon/dependency_graph/InitializationOrderDependencyUtils.java
@@ -90,12 +90,11 @@ final class InitializationOrderDependencyUtils {
                         new ProviderCandidate(candidateProviderMember, requireSourceStart(candidateProviderMember)))
                 .filter(candidate -> candidate.getSourceStart() < dependentSourceStart)
                 .map(ProviderCandidate::getProviderMember)
-                .filter(providerMember ->
-                        isFieldWrittenInInitializationRootByMember(providerMember, blankFinalField))
+                .filter(providerMember -> isFieldWrittenByMember(providerMember, blankFinalField))
                 .collect(Collectors.toUnmodifiableSet());
     }
 
-    private static boolean isFieldWrittenInInitializationRootByMember(
+    private static boolean isFieldWrittenByMember(
             @NonNull CtTypeMember candidateProviderMember, @NonNull CtField<?> blankFinalField) {
         return resolveInitializationAstRoot(candidateProviderMember)
                 .map(astRoot ->
