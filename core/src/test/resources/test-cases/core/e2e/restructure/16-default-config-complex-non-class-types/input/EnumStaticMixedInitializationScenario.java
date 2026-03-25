28fe9b4b170f7b831bd2842a8c33e3bdbef1558e
AntonLem <antonlem78@gmail.com>
2026-03-25 16:54:01 +0100

    Add enum mixed static initialization e2e fixture

---
+++ core/src/test/resources/test-cases/core/e2e/restructure/16-default-config-complex-non-class-types/input/EnumStaticMixedInitializationScenario.java
@@ -0,0 +1,32 @@
+package io.github.lemon_ant.jharmonizer.core.e2e;
+
+public enum EnumStaticMixedInitializationScenario {
+    SECOND("second"),
+    FIRST("first");
+
+    private final String label;
+
+    private static final String bDependent = resolveDependent();
+
+    private EnumStaticMixedInitializationScenario(String label) {
+        this.label = label;
+    }
+
+    private static String zProvider = "provider";
+
+    private static final String aEnumSnapshot = FIRST.label + ":" + SECOND.label;
+
+
+    private static String resolveDependent() {
+        return zProvider + "-dep";
+    }
+
+    public static void main(String[] args) {
+        if (!"null-dep".equals(bDependent)) {
+            throw new IllegalStateException("Mixed static initialization order changed: " + bDependent);
+        }
+        if (!"first:second".equals(aEnumSnapshot)) {
+            throw new IllegalStateException("Enum constant snapshot changed: " + aEnumSnapshot);
+        }
+    }
+}
