08c95dcd6c7fd498cb11ddf85e2a2b4688d25650
AntonLem <antonlem78@gmail.com>
2026-04-02 19:08:31 +0200

    Move two-field forward-reference case to restructure fixtures

--- core/src/test/resources/test-cases/core/e2e/regression/05-date-cast-forward-reference/expected/DateCastEvaluator.java
+++
@@ -1,9 +0,0 @@
-package org.apache.nifi.attribute.expression.language.evaluation.cast;
-
-import java.time.format.DateTimeFormatter;
-
-public class DateCastEvaluator {
-    public static final String ALTERNATE_FORMAT_WITHOUT_MILLIS = "yyyy/MM/dd HH:mm:ss";
-    public static final DateTimeFormatter ALTERNATE_FORMATTER_WITHOUT_MILLIS =
-            DateTimeFormatter.ofPattern(ALTERNATE_FORMAT_WITHOUT_MILLIS);
-}
